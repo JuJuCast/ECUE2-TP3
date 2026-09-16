3058a463027728cd1fe02b2eb5c2e46fb963b523
Julien Forestier <julien.forestier.auditeur@lecnam.net>
2026-09-16 16:48:38 +0200

    Exercice 2

---
+++ src/net/lecnam/ecue2/tp3/Rectangle.java
@@ -0,0 +1,14 @@
+package net.lecnam.ecue2.tp3;
+
+public class Rectangle {
+    double longueur;
+    double largeur;
+    Point origine;
+
+    public Rectangle(Point origine, double longueur, double largeur) {
+        this.origine = origine;
+        this.longueur = longueur;
+        this.largeur = largeur;
+    }
+}
+
